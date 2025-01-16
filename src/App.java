5d24dd37f65fd03534db5a7e3b1f154724a5f916
julianrodzs <49877813+julianrodzs@users.noreply.github.com>
2025-01-15 23:16:14 -0600

    arreglos y operadores logicos

--- src/App.java
+++ src/App.java
@@ -20,12 +20,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         //PASO 1: Generar el lexer y parser
-       GenerarLexerParser(); //comienza a generar el lexer y parser    
+       //GenerarLexerParser(); //comienza a generar el lexer y parser    
         
         //PASO 2: Iniciar el lexer
         // String ruta = "C:\\Users\\mauri\\OneDrive\\Documentos\\GitHub\\lexer\\src\\texto\\codigo.txt";
-        // String ruta = "C:\\Users\\julil\\OneDrive\\Documentos\\GitHub\\lexer\\src\\texto\\codigo.txt";
-        String ruta = "";
+        String ruta = "C:\\Users\\julil\\OneDrive\\Documentos\\GitHub\\lexer\\src\\texto\\codigo.txt";
+        //String ruta = "";
         //espera 4 segundos
         Thread.sleep(5000);
         //PASO 3 : Ejecutar el parser
